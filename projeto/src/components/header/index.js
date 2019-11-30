b1cb5c2eb9c417ae837c177673b000ab1ac3fb0a
woman <woman@info.local>
2019-11-30 11:06:36 -0300

    header iniciado, pastas arrumadas

---
+++ projeto/src/components/header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import '../../styles/pages/home';
+import Logo from '../../assets/images/logo_teste.png';
+
+function Header(){
+    return(
+        <div className="header">
+            <img src={Logo} />
+            Header funcionando
+        </div>
+    )
+}
+export default Header;
